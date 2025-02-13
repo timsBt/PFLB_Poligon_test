4a0706e318994b8125d78d674b15ab056dc6945b
Timur Bekmatov <tims.bekmat@gmail.com>
2025-02-14 02:56:29 +0300

    headless true

--- src/test/java/tests/uiTests/BaseTest.java
+++ src/test/java/tests/uiTests/BaseTest.java
@@ -20,7 +20,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.timeout = 10000;
         open(System.getProperty("url", getProperty("url")));
         getWebDriver().manage().window().maximize();
