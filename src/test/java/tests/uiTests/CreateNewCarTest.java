3d38cad22fd14c15e74d3124457db3bb22fef7d7
VBrokert <brokertva@gmail.com>
2025-02-17 23:33:24 +0700

    Исправил тетс удаления автомобиля. Добавил тест удаления не существующего автомобиля. Убрал retryAnalyzer = RetryUtils.class. Заменил softAssert

--- src/test/java/tests/uiTests/CreateNewCarTest.java
+++ src/test/java/tests/uiTests/CreateNewCarTest.java
@@ -4,10 +4,10 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import utils.RetryUtils;
 
 import static com.codeborne.selenide.Condition.text;
@@ -16,7 +16,6 @@ import static com.codeborne.selenide.Condition.text;
 public class CreateNewCarTest extends BaseTest {
 
     String carId = "";
-    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void createCarPreparation() {
@@ -34,12 +33,12 @@ public class CreateNewCarTest extends BaseTest {
                 "VedroS",
                 "Gaykamy",
                 "1000");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: Successfully pushed, code: 201",
                 "Возникла ошибка при создании автомобиля");
     }
 
-    @Test(priority = 1, testName = "Тест удаления созданного автомобиля", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 1, testName = "Тест удаления созданного автомобиля")
     @Description("Тест удаления созданного автомобиля")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -49,12 +48,20 @@ public class CreateNewCarTest extends BaseTest {
                 "Gaykamy",
                 "1000");
         allDeletePage.deleteCarId(carId);
-        softAssert.assertEquals(createCar.carCreateStatus(),
-                "Status: 204",
-                "Возникла ошибка при удалении автомобиля");
+        allDeletePage.deleteCarStatus.shouldHave(text("204"));
     }
 
-    @Test(priority = 2, testName = "Тест создания автомобиля с пустым полем Engine Type", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 1, testName = "Тест удаления не существующего автомобиля")
+    @Description("Тест удаления не существующего автомобиля")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
+    public void deleteNewCarWithNotValidData() {
+        carId = "9999";
+        allDeletePage.deleteCarId(carId);
+        allDeletePage.deleteCarStatus.shouldHave(text("Status: not pushed"));
+    }
+
+    @Test(priority = 2, testName = "Тест создания автомобиля с пустым полем Engine Type")
     @Description("Тест создания автомобиля с пустым полем Engine Type")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -63,12 +70,12 @@ public class CreateNewCarTest extends BaseTest {
                 "VedroS",
                 "Gaykamy",
                 "1000");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
     }
 
-    @Test(priority = 3, testName = "Тест создания автомобиля с не валидными данными в поле Engine Type", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 3, testName = "Тест создания автомобиля с не валидными данными в поле Engine Type")
     @Description("Тест создания автомобиля с не валидными данными в поле Engine Type")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -77,12 +84,12 @@ public class CreateNewCarTest extends BaseTest {
                 "VedroS",
                 "Gaykamy",
                 "1000");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: AxiosError: Request failed with status code 400",
                 "Сообщение об ошибке отсутствует");
     }
 
-    @Test(priority = 4, testName = "Тест создания автомобиля с пустым полем Mark", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 4, testName = "Тест создания автомобиля с пустым полем Mark")
     @Description("Тест создания автомобиля с пустым полем Mark")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -91,12 +98,12 @@ public class CreateNewCarTest extends BaseTest {
                 "",
                 "Gaykamy",
                 "1000");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
     }
 
-    @Test(priority = 5, testName = "Тест создания автомобиля с пустым полем Model", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 5, testName = "Тест создания автомобиля с пустым полем Model")
     @Description("Тест создания автомобиля с пустым полем Model")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -105,12 +112,12 @@ public class CreateNewCarTest extends BaseTest {
                 "VedroS",
                 "",
                 "1000");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
     }
 
-    @Test(priority = 6, testName = "Тест создания автомобиля с пустым полем Price", retryAnalyzer = RetryUtils.class)
+    @Test(priority = 6, testName = "Тест создания автомобиля с пустым полем Price")
     @Description("Тест создания автомобиля с пустым полем Price")
     @Feature("Взаимодействие с автомобилем")
     @Story("Создание нового автомобиля")
@@ -119,7 +126,7 @@ public class CreateNewCarTest extends BaseTest {
                 "VedroS",
                 "Gaykamy",
                 "");
-        softAssert.assertEquals(createCar.carCreateStatus(),
+        Assert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
     }
