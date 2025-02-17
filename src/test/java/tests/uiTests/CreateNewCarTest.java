f6460f24451848d31d9110fd03e72489eaf5f48d
VBrokert <brokertva@gmail.com>
2025-02-17 20:55:17 +0700

    Добавил аннотации и тест удаления автомобиля

--- src/test/java/tests/uiTests/CreateNewCarTest.java
+++ src/test/java/tests/uiTests/CreateNewCarTest.java
@@ -1,5 +1,9 @@
 package tests.uiTests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,10 +12,10 @@ import utils.RetryUtils;
 
 import static com.codeborne.selenide.Condition.text;
 
+@Epic("UI tests")
 public class CreateNewCarTest extends BaseTest {
 
     String carId = "";
-    String carStatusError = "";
     SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
@@ -22,6 +26,9 @@ public class CreateNewCarTest extends BaseTest {
     }
 
     @Test(priority = 1, testName = "Тест создания автомобиля с валидными данными", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с валидными данными")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithValidData() {
         createCar.createNewCar("Gasoline",
                 "VedroS",
@@ -33,7 +40,26 @@ public class CreateNewCarTest extends BaseTest {
         carId = createCar.carIdGet();
     }
 
+    @Test(priority = 1, testName = "Тест удаления созданного автомобиля", retryAnalyzer = RetryUtils.class)
+    @Description("Тест удаления созданного автомобиля")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
+    public void deleteNewCarWithValidData() {
+        createCar.createNewCar("Gasoline",
+                "VedroS",
+                "Gaykamy",
+                "1000");
+        carId = createCar.carIdGet();
+        allDeletePage.deleteCarId(carId);
+        softAssert.assertEquals(createCar.carCreateStatus(),
+                "Status: 204",
+                "Возникла ошибка при удалении автомобиля");
+    }
+
     @Test(priority = 2, testName = "Тест создания автомобиля с пустым полем Engine Type", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с пустым полем Engine Type")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithEmptyEngineTypeField() {
         createCar.createNewCar("",
                 "VedroS",
@@ -42,10 +68,12 @@ public class CreateNewCarTest extends BaseTest {
         softAssert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
-        carStatusError = createCar.carCreateStatus();
     }
 
     @Test(priority = 3, testName = "Тест создания автомобиля с не валидными данными в поле Engine Type", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с не валидными данными в поле Engine Type")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithNotValidEngineTypeField() {
         createCar.createNewCar("sdgwegwge",
                 "VedroS",
@@ -54,10 +82,12 @@ public class CreateNewCarTest extends BaseTest {
         softAssert.assertEquals(createCar.carCreateStatus(),
                 "Status: AxiosError: Request failed with status code 400",
                 "Сообщение об ошибке отсутствует");
-        carStatusError = createCar.carCreateStatus();
     }
 
     @Test(priority = 4, testName = "Тест создания автомобиля с пустым полем Mark", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с пустым полем Mark")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithEmptyMarkField() {
         createCar.createNewCar("Electric",
                 "",
@@ -66,10 +96,12 @@ public class CreateNewCarTest extends BaseTest {
         softAssert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
-        carStatusError = createCar.carCreateStatus();
     }
 
     @Test(priority = 5, testName = "Тест создания автомобиля с пустым полем Model", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с пустым полем Model")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithEmptyModelField() {
         createCar.createNewCar("Diesel",
                 "VedroS",
@@ -78,10 +110,12 @@ public class CreateNewCarTest extends BaseTest {
         softAssert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
-        carStatusError = createCar.carCreateStatus();
     }
 
     @Test(priority = 6, testName = "Тест создания автомобиля с пустым полем Price", retryAnalyzer = RetryUtils.class)
+    @Description("Тест создания автомобиля с пустым полем Price")
+    @Feature("Взаимодействие с автомобилем")
+    @Story("Создание нового автомобиля")
     public void createNewCarWithEmptyPriceField() {
         createCar.createNewCar("CNG",
                 "VedroS",
@@ -90,16 +124,13 @@ public class CreateNewCarTest extends BaseTest {
         softAssert.assertEquals(createCar.carCreateStatus(),
                 "Status: Invalid request data",
                 "Сообщение об ошибке отсутствует");
-        carStatusError = createCar.carCreateStatus();
     }
 
     @AfterMethod
     public void deleteCar() {
-        if ("Status: Invalid request data".equals(carStatusError) ||
-                "Status: AxiosError: Request failed with status code 400".equals(carStatusError)) {
-            return; // Прерываем выполнение метода, если carStatusError равно одной из указанных строк
+        if (!carId.isEmpty() && !carId.equals("9999")) {
+            allDeletePage.deleteCarId(carId);
+            allDeletePage.deleteCarStatus.shouldHave(text("204"));
         }
-        allDeletePage.deleteCarId(carId);
-        allDeletePage.deleteCarStatus.shouldHave(text("Status: 204"));
     }
 }
