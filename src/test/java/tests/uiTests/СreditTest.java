ec2d2f0b469bc1587ec279b6c647d16c12dd84d8
Timur Bekmatov <tims.bekmat@gmail.com>
2025-02-24 19:50:25 +0300

    Добавил Allure аннотации на тесты Добавление денег и взятие кредита. Отрефакторил тесты в связи с изменениями в BaseTest

--- src/test/java/tests/uiTests/СreditTest.java
+++ src/test/java/tests/uiTests/СreditTest.java
@@ -1,16 +1,14 @@
 package tests.uiTests;
 
-import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.MainPage;
-import pages.PlusMoneyPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.open;
 import static utils.PropertyReader.getProperty;
+
 @Epic("UI tests")
 public class СreditTest extends BaseTest {
 
@@ -18,10 +16,10 @@ public class СreditTest extends BaseTest {
 
     @BeforeMethod
     public void openCredit() {
-        mainPage.authorization()
+        mainPage.authorization(login, password)
                 .toggleNavigationClick("Users")
                 .selectDropDownMenu("Create new");
-        userId = createUser.createNewUser(getProperty("firstName"), getProperty("lastName"), getProperty("age"),
+        userId = createUserPage.createNewUser(getProperty("firstName"), getProperty("lastName"), getProperty("age"),
                 getProperty("sex"), getProperty("money"));
         mainPage.toggleNavigationClick("Users")
                 .selectDropDownMenu("Issue a loan");
@@ -29,6 +27,8 @@ public class СreditTest extends BaseTest {
 
     @Test(priority = 1, testName = "Проверка на взятие кредита.",
             description = "Ввод корректного id и суммы.")
+    @Feature("Взятие кредита")
+    @Story("Ввод корректного id и суммы.")
     public void testLoanFailure() {
         plusMoneyPage.enterUserId(userId).enterAmount("100000").submit()
                 .verifySuccessMessage("Status: AxiosError: Request failed with status code 408");
@@ -36,6 +36,8 @@ public class СreditTest extends BaseTest {
 
     @Test(priority = 2, testName = "Проверка на кредит несуществующему пользователю.",
             description = "Ввод некорректного id и суммы.")
+    @Feature("Взятие кредита")
+    @Story("Ввод некорректного id и суммы.")
     public void testAddMoneyNonExistentUser() {
         plusMoneyPage.enterUserId("2180000").enterAmount("1000").submit()
                 .verifySuccessMessage("Status: AxiosError: Request failed with status code 404");
@@ -43,13 +45,15 @@ public class СreditTest extends BaseTest {
 
     @Test(priority = 3, testName = "Проверка на кредит некорректной суммы.",
             description = "Ввод корректного id и отрицательной суммы.")
+    @Feature("Взятие кредита")
+    @Story("Ввод корректного id и отрицательной суммы.")
     public void testEnteringIncorrectAmount() {
         plusMoneyPage.enterUserId(userId).enterAmount("-1000").submit()
                 .verifySuccessMessage("Status: Incorrect input data");
     }
+
     @AfterMethod
     public void deleteUser() {
         allDeletePage.deleteUserId(userId);
-        allDeletePage.deleteUserStatus.shouldHave(text("Status: 204"));
     }
 }
