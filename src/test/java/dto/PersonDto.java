8a6bfc182205c7be56268a44287f01f5133ba35d
Aleksandra Parshina <coyote7.p@gmail.com>
2025-02-25 20:02:45 +0500

    feature/ промежуточный

--- src/test/java/dto/PersonDto.java
+++ src/test/java/dto/PersonDto.java
@@ -1,26 +1,17 @@
 package dto;
 
-import io.qameta.allure.internal.shadowed.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.ArrayList;
 
 @Data
 public class PersonDto {
-    @JsonProperty("id")
     public int id;
-    @JsonProperty("first_name")
     public String firstName;
-    @JsonProperty("second_name")
     public String secondName;
-    @JsonProperty("age")
     public int age;
-    @JsonProperty("sex")
     public String sex;
-    @JsonProperty("money")
     public double money;
-    @JsonProperty("house")
     public Object house;
-    @JsonProperty("cars")
     public ArrayList<CarDto> cars;
 }
--- src/test/java/tests/apiTests/SellAndBuyCarApiTest.java
+++ src/test/java/tests/apiTests/SellAndBuyCarApiTest.java
@@ -1,9 +1,19 @@
 package tests.apiTests;
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 
 public class SellAndBuyCarApiTest {
 
     @Test
-    public void SellAndBuyCar() {}
+    public void SellAndBuyCar() {
+        String response = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get()
+    }
 }
