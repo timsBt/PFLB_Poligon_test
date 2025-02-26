2db3a85e7b8f47074896800c27cfac1b3a484b6a
Aleksandra Parshina <coyote7.p@gmail.com>
2025-02-26 12:26:11 +0500

    feature/ добавление BaseClassApiTest. Добавление проверки количества машин у юзера. Добавление метода setUp()

--- src/test/java/tests/apiTests/GetCarsListFromUserApiTest.java
+++ src/test/java/tests/apiTests/GetCarsListFromUserApiTest.java
@@ -1,6 +1,5 @@
 package tests.apiTests;
 
-import adapters.AuthAdapter;
 import dto.PersonDto;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
@@ -9,19 +8,18 @@ import models.UserDto;
 import org.testng.annotations.Test;
 import service.OperationWithCar;
 import service.RestService;
-import tests.uiTests.BaseTest;
 
-public class GetCarsListFromUserApiTest extends BaseTest {
+public class GetCarsListFromUserApiTest extends BaseClassApiTest {
 
     @Test(testName = "API: Получение списка машин по userId",
             description = "Проверка, что API возвращает список машин")
-    @Description("Сравнение списка машин пользователя, полученного из UI, с ответом API")
+    @Description("Сравнение списка машин пользователя")
     @Feature("API-тестирование")
     @Story("GET /cars?userId={userId}")
     public void getCarsListApiTest() {
         RestService restService = new RestService();
-        String token = AuthAdapter.getToken();
         PersonDto getListCars = restService.getUserWithCars("1");
+        checkUserCars(getListCars, 2);
         UserDto sellCar =  restService.buyOrSellCar("1", "1", OperationWithCar.BUY, token);
     }
 }
