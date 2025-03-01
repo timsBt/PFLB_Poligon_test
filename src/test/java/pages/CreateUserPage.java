1864b5fc921ce4f414e6d14b130cecdb6b8d67b5
Timur Bekmatov <tims.bekmat@gmail.com>
2025-03-01 20:08:07 +0300

    добавил зависимости. Добавил log4j2 файл. Добавил логирование на создание юзера

--- src/test/java/pages/CreateUserPage.java
+++ src/test/java/pages/CreateUserPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -9,6 +10,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 import static org.openqa.selenium.By.id;
 
+@Log4j2
 public class CreateUserPage {
 
     private static final SelenideElement FIRST_NAME_SEND = $(id("first_name_send"));
@@ -20,8 +22,9 @@ public class CreateUserPage {
     private static final SelenideElement STATUS = $x("//button[@class = 'status btn btn-secondary']");
     private static final String SEX_SEND = "//input[@value = '%s']";
 
-    @Step("Создание нового юзера c полями: {firstName}, {lastName}, {age}, {sex}, {money}")
+    @Step("Создание нового юзера c данными: {firstName}, {lastName}, {age}, {sex}, {money}")
     public String createNewUser(String firstName, String lastName, String age, String sex, String money) {
+        log.info("Создание нового юзера c данными: '{}', '{}', '{}', '{}', '{}'", firstName, lastName, age, sex, money);
         FIRST_NAME_SEND.shouldBe(visible, ofSeconds(10)).setValue(firstName);
         LAST_NAME_SEND.setValue(lastName);
         AGE_SEND.setValue(age);
@@ -36,6 +39,7 @@ public class CreateUserPage {
 
     @Step("Проверка статуса: {actualResult}")
     public void checkStatus(String actualResult) {
+        log.info("Проверка статуса: '{}'", actualResult);
         STATUS.shouldBe(visible, ofSeconds(10)).shouldHave(text(actualResult));
     }
 }
