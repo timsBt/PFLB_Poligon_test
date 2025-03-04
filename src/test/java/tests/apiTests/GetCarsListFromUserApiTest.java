e31bbf31d3a9a55e4d7312c7d954d462c038a46b
Aleksandra Parshina <coyote7.p@gmail.com>
2025-03-04 13:40:00 +0500

    feature/рефакторинг теста GetCarsListFromUserApiTest

--- src/test/java/tests/apiTests/GetCarsListFromUserApiTest.java
+++ src/test/java/tests/apiTests/GetCarsListFromUserApiTest.java
@@ -1,11 +1,12 @@
 package tests.apiTests;
 
+import adapters.CarAdapter;
+import adapters.UserAdapter;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import models.userModels.PersonDto;
-import models.userModels.UserDto;
 import org.testng.annotations.Test;
 import service.OperationWithCar;
 import service.RestService;
@@ -20,8 +21,32 @@ public class GetCarsListFromUserApiTest extends BaseClassApiTest {
     @Story("GET /cars?userId={userId}")
     public void getCarsListApiTest() {
         RestService restService = new RestService();
-        PersonDto getListCars = restService.getUserWithCars("1");
+
+        // Читаем данные из пропертей
+        String firstName = properties.getProperty("firstName");
+        String lastName = properties.getProperty("lastName");
+        String age = properties.getProperty("age");
+        String sex = properties.getProperty("sex");
+        String money = properties.getProperty("money");
+
+        String engineType = properties.getProperty("engineType");
+        String mark = properties.getProperty("mark");
+        String model = properties.getProperty("model");
+        String price = properties.getProperty("price");
+
+        // Создание пользователя
+        String userId = UserAdapter.createUser(firstName, lastName, age, sex, money);
+
+        // Создание двух машин
+        String carId1 = CarAdapter.createCar(engineType, mark, model, price);
+        String carId2 = CarAdapter.createCar(engineType, "BMW", "X5", "30000");
+
+        // Покупка машин пользователем
+        restService.buyOrSellCar(userId, carId1, OperationWithCar.BUY, token);
+        restService.buyOrSellCar(userId, carId2, OperationWithCar.BUY, token);
+
+        // Получение списка машин у пользователя и проверка
+        PersonDto getListCars = restService.getUserWithCars(userId);
         checkUserCars(getListCars, 2);
-        UserDto sellCar = restService.buyOrSellCar("1", "1", OperationWithCar.BUY, token);
     }
 }
